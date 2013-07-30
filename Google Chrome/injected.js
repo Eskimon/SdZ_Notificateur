3636fa6de45c754878a38db08a8c351755be857b
Eskimon <eskimon@outlook.com>
2013-07-30 12:04:28 +0200

    Correction d'une typo dans inject.js

--- Google Chrome/injected.js
+++ Google Chrome/injected.js
@@ -13,6 +13,6 @@ for (var i = 0; i < len; i++) {
 target && target.click();
 
 // Update du nombre de notifs
-var elem = document.getElementById("#notifucations");
+var elem = document.getElementById("#notifications");
 elem.click();
 elem.click();
