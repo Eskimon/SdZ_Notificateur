6ff670448fcb3415abaeeb8192367feb18a0276a
AndroWiiid <androwiiid@gmail.com>
2013-10-06 22:41:49 +0200

    Add library to parse homepage of openclassrooms.

--- Android/src/fr/oc/ocnotification/network/NotificationManager.java
+++ Android/src/fr/oc/ocnotification/network/NotificationManager.java
@@ -1,25 +1,22 @@
 package fr.oc.ocnotification.network;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import fr.oc.ocnotification.models.Notification;
-import fr.oc.ocnotification.utils.Utils;
 
+// TODO Must implement this method.
 public class NotificationManager {
-	private final static String URL = "http://fr.openclassrooms.com/";
-	private final WebService mWebService = new WebService();
+	// private final static String URL = "http://fr.openclassrooms.com/";
+	// private final WebService mWebService = new WebService();
 
 	public List<Notification> downloadNotifications() {
-		final InputStream is = mWebService.connect(URL);
-		final List<Notification> res = parse(is);
+		// final InputStream is = mWebService.connect(URL);
+		// final List<Notification> res = parse(is);
+		
+		// Impossible de recuperer les notifications. Utilisateur non connecte
+		// d'office.
 		return new ArrayList<Notification>();
-	}
-
-	private List<Notification> parse(InputStream is) {
-		final String response = Utils.convertInputStreamToString(is);
-		return null;
 	}
 
 }
